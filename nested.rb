d1864a194ea3a6312bd5af7c3280768e2b1be1c2
Kizito Njoku <nkizito57@gmail.com>
2019-11-09 05:07:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,6 +128,10 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+<<<<<<< HEAD
  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+=======
+ programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+>>>>>>> 631fae0717270f1b5b4bcadb66ba26cdeb2b869a
   programmer_hash
 end
